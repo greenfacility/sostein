172999c6f26b81eb31d1400335d3bbae69240778
Green Sostein <greenfacilityltd@gmail.com>
2020-04-07 14:59:24 +0100

    Finished version cln

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
-	// MongoURI: 'mongodb+srv://eliteaddy:adeshile@cluster0-ufp8b.mongodb.net/green-test?retryWrites=true',
-	// MongoURI: 'mongodb://localhost/green-test',
 	MongoURI:
 		'mongodb+srv://greenfacility:Greenfacility@1@cluster0-chydu.mongodb.net/sostein?retryWrites=true&w=majority',
 	jwtSecret: 'anythingIsSecret',
